cc15d4b6865303d9a002d6dab1701155fd26c6c6
James Noeckel <jamesn8@uw.edu>
2019-01-25 01:22:49 -0800

    refactored

--- canvas.py
+++ canvas.py
@@ -1,4 +1,5 @@
 import numpy as np
+from CSG import *
 
 def compute_distance(grid, states, y, x, h, debug_map=None):
     #find the upwind values along each dimension
@@ -50,60 +51,6 @@ def partialx(x):
     result[:,-1] = x[:,-1] - x1[:,-1]
     result[:,1:-1] = (x2[:,1:-1] - x1[:,1:-1])*0.5
     return result
-
-class CSG(object):
-    def __init__(self):
-        self.rotation = np.eye(2)
-        self.translation = np.zeros(2)
-    def __call__(self, pos):
-        if not hasattr(self, '_map'):
-            raise ValueError('CSG solid does not implement map function')
-        return self._map(self._transform(pos))
-    def _transform(self, pos):
-        return self.rotation.dot(pos - self.translation)
-    def set_rotation(self, angle):
-        c = np.cos(-angle)
-        s = np.sin(-angle)
-        self.rotation = np.array([[c, -s], [s, c]])
-    def set_translation(self, offset):
-        self.translation = np.array(offset)
-
-class Union(CSG):
-    def __init__(self, *shapes):
-        super(Union, self).__init__()
-        self.shapes = shapes
-    def _map(self, pos):
-        return min([shape(pos) for shape in self.shapes])
-
-class Intersection(CSG):
-    def __init__(self, *shapes):
-        super(Intersection, self).__init__()
-        self.shapes = shapes
-    def _map(self, pos):
-        return max([shape(pos) for shape in self.shapes])
-
-class Negation(CSG):
-    def __init__(self, shape):
-        super(Negation, self).__init__()
-        self.shape = shape
-    def _map(self, pos):
-        return -self.shape(pos)
-
-class Circle(CSG):
-    def __init__(self, radius):
-        super(Circle, self).__init__()
-        self.radius = radius
-    def _map(self, pos):
-        return np.sqrt(pos[0]*pos[0]+pos[1]*pos[1]) - self.radius
-
-class Square(CSG):
-    def __init__(self, radius):
-        super(Square, self).__init__()
-        self.radius = radius
-    def _map(self, pos):
-        inside = np.max(np.abs(pos)) - self.radius
-        disp = np.clip(np.abs(pos)-self.radius, 0, None)
-        return min(inside, 0) + np.sqrt(disp.dot(disp))
 
 
 class Canvas:
