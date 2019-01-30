a78873432d78d0899fe241da090e2b7cf5336908
James Noeckel <jamesn8@uw.edu>
2019-01-29 18:24:01 -0800

    changed how transforms work

--- CSG.jl
+++ CSG.jl
@@ -1,65 +1,49 @@
-struct Transform{T <: AbstractFloat}
+abstract type CSG{T <: AbstractFloat} end
+
+struct Transform{T} <: CSG{T}
+    child :: CSG
     x :: T
     y :: T
     rotation :: T
 end
-Transform(x=0.0, y=0.0, r=0.0) = Transform{Float64}(x, y, r)
-
-function (trans::Transform)(posx::Real, posy::Real)
-    c = cos(trans.rotation)
-    s = sin(trans.rotation)
-    posx, posy = (posx-trans.x, posy-trans.y)
-    newpos = (c * posx + s * posy,
-              -s * posx + c * posy)
-    return newpos
-end
-
-
-abstract type CSG{T <: AbstractFloat} end
+Transform(child::CSG, x::Real, y::Real, r::Real=0.0) = Transform{Float64}(child, x, y, r)
 
 struct Circle{T} <: CSG{T}
     radius :: T
-    transform :: Transform{T}
 end
 
 struct Square{T} <: CSG{T}
     radius :: T
-    transform :: Transform{T}
 end
 
 #composite CSG types
 struct Intersect{T} <: CSG{T}
     children :: Vector{CSG{T}}
-    transform :: Transform{T}
 end
 struct Union{T} <: CSG{T}
     children :: Vector{CSG{T}}
-    transform :: Transform{T}
 end
 struct Negate{T} <: CSG{T}
     child :: CSG{T}
 end
 
-_map(csg::CSG, pos::Vararg{Real, 2}) = error("_map not implemented for $(typeof(csg))")
 import Base.getindex
-function getindex(csg::T, posx::Real, posy::Real) where {T <: CSG}
-    if @generated
-        if in(:transform, fieldnames(T))
-            return :(_map(csg, csg.transform(posx, posy)...))
-        else
-            return :(_map(csg, posx, posy))
-        end
-    else
-        return _map(csg, (in(:transform, fieldnames(T)) ? csg.transform(posx, posy) : (posx, posy))...)
-    end
+function getindex(trans::Transform, posx::Real, posy::Real)
+    c = cos(trans.rotation)
+    s = sin(trans.rotation)
+    posx, posy = (posx-trans.x, posy-trans.y)
+    newposx, newposy = (c * posx + s * posy,
+                       -s * posx + c * posy)
+    return trans.child[newposx, newposy]
 end
+getindex(csg::T, posx::Real, posy::Real) where {T <: CSG} = error("getindex not implemented for $(typeof(csg))")
 #signed distance functions
-_map(csg::Circle, posx::Real, posy::Real) = sqrt(posx*posx+posy*posy) - csg.radius
-function _map(csg::Square, pos::Vararg{Real, 2})
+getindex(csg::Circle, posx::Real, posy::Real) = sqrt(posx*posx+posy*posy) - csg.radius
+function getindex(csg::Square, pos::Vararg{Real, 2})
     inside = max(abs.(pos)...) - csg.radius
     disp = max.(abs.(pos) .- csg.radius, 0)
     return min(inside, 0) + sqrt(disp[1]*disp[1]+disp[2]*disp[2])
 end
-_map(csg::Intersect, pos::Vararg{Real, 2}) = maximum(child[pos...] for child in csg.children)
-_map(csg::Union, pos::Vararg{Real, 2}) = minimum(child[pos...] for child in csg.children)
-_map(csg::Negate, pos::Vararg{Real, 2}) = -csg.child[pos...]
+getindex(csg::Intersect, pos::Vararg{Real, 2}) = maximum(child[pos...] for child in csg.children)
+getindex(csg::Union, pos::Vararg{Real, 2}) = minimum(child[pos...] for child in csg.children)
+getindex(csg::Negate, pos::Vararg{Real, 2}) = -csg.child[pos...]
