fdc04cda04162202864e8ac4f2438fc90dc9d58f
James Noeckel <jamesn8@uw.edu>
2019-02-22 20:02:30 -0800

    added plotting to test

--- interpretertest.jl
+++ interpretertest.jl
@@ -2,8 +2,28 @@ using PyCall
 
 ex = pyimport("examples")
 
-cmds = ex[:getCmds3]()
+cmds = ex[:getCmds1]()
 
 include("interpreter.jl")
 
 result = execute(cmds)
+
+using Images
+function display_normals!(image::Matrix{RGB{T}}, normals::Array{T,3}, dists::Matrix{T}) where {T <: AbstractFloat}
+    for I in CartesianIndices(image)
+        rg = @view normals[:,I]
+        dist = dists[I]
+        fac = dist < 0 ? 0.5 : 1.0
+        t(x) = (x*0.3+0.5)*fac
+        image[I] = RGB(t(rg[1]), t(rg[2]), fac*0.5)
+    end
+    return image
+end
+function display_normals(normals::Array{T,3}, dists::Matrix{T}) where {T <: AbstractFloat}
+    image = Array{RGB{T}}(undef, size(normals)[2:end]...)::Array{RGB{T}, 2}
+    return display_normals!(image, normals, dists)
+end
+display_normals(canvas::Canvas) = display_normals(canvas.normals, canvas.grid)
+
+image = display_normals(result)
+display(image)
