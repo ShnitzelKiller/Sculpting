2b7fb1049ad051f7f64db600962139eb5e53096b
James Noeckel <jamesn8@uw.edu>
2019-03-08 18:17:20 -0800

    changed examples

--- examples.py
+++ examples.py
@@ -7,11 +7,12 @@ def getCmds1():
 	for n in range(5):
 		scale = 1.0/(n+1)
 		shape = Union(shape, Translate(Scale(Circle(), Vec2(scale,scale)), Vec2(0, n)))
-	#shape2 = Scale(Square(), Vec2(0.2, 5))
+	shape2 = Scale(Square(), Vec2(0.2, 5))
+	shape2 = Union(shape2, shape)
 	#field = SolidToField(shape2)
 	#shape=ExpandSurface(shape, field, 0.2)
 
-	return Output(shape)
+	return Output(shape2)
 
 def getCmds2():
 	shape = Circle()
@@ -22,7 +23,8 @@ def getCmds2():
 		shape = Union(shape, Translate(cog, Vec2(cos(ang), sin(ang))))
 	field = UniformField(1)
 	shape2 = ExpandSurface(shape, field, -0.2)
-	return Output(shape2)
+	shape3 = Translate(shape2, Vec2(0, 3))
+	return Output(Union(shape2, shape3))
 
 def getCmds3():
 	shape = Circle()
