e41b9130116e8a0c5373e563c58d37226f491d5e
James Noeckel <jamesn8@uw.edu>
2019-03-20 22:01:43 -0700

    updated examples

--- examples.py
+++ examples.py
@@ -14,20 +14,28 @@ def getCmds1():
 
 	return Output(shape2)
 
+def replicate(branch, thickness=0.2, vee=Translate(Scale(Square(), Vec2(0.2, 1)), Vec2(0, -1))):
+	piece = Union(vee, Translate(branch, Vec2(0, -2)))
+	left = Rotate(piece, 30)
+	right = Rotate(piece, -30)
+	return Union(left, right, Scale(Circle(), Vec2(thickness, thickness)))
+
 def getCmds2():
 	shape = Circle()
-	num = 6
+	num = 12
 	for i in range(num):
 		ang = i/num * 6.28
 		cog = Scale(Circle(), Vec2(0.4, 0.4))
 		shape = Union(shape, Translate(cog, Vec2(cos(ang), sin(ang))))
 	field = UniformField(1)
 	shape2 = ExpandSurface(shape, field, -0.2)
-	shape2 = Translate(shape2, Vec2(0, 1))
-	shape3 = shape2
-	for i in range(num):
-		ang = i/num*360
-		shape3 = Union(shape3, Rotate(shape2, ang))
+	#shape2 = Translate(shape2, Vec2(0, 1))
+	#shape3 = shape2
+	#for i in range(num):
+	#	ang = i/num*360
+	#	shape3 = Union(shape3, Rotate(shape2, ang))
+	shape3 = replicate(Scale(shape2, Vec2(0.75, 0.75)))
+
 	return Output(shape3)
 
 def getCmds3():
@@ -39,18 +47,21 @@ def getCmds3():
 def getCmds4():
 	thickness = 0.4
 	vee = Translate(Scale(Square(), Vec2(thickness, 1)), Vec2(0, -1))
-	#vee = Union(vee, Translate(Scale(Circle(), Vec2(thickness, thickness)), Vec2(0, 2)))
-	def replicate(branch):
-		piece = Union(vee, Translate(branch, Vec2(0, -2)))
-		left = Rotate(piece, 30)
-		right = Rotate(piece, -30)
-		return Union(left, right, Scale(Circle(), Vec2(thickness, thickness)))
-
-	tree = replicate(Scale(Circle(), Vec2(0.75, 0.75)))
+	tree = replicate(Scale(Circle(), Vec2(0.75, 0.75)), thickness, vee)
 	fac = 0.75
 	f = UniformField(1)
 	for i in range(4):
-		tree = replicate(Scale(tree, Vec2(1, fac)))
+		tree = replicate(Scale(tree, Vec2(1, fac)), thickness, vee)
 		tree = ExpandSurface(tree, f, -0.05)
+
+	return Output(tree)
+
+def getCmds5(): #tree for testing performance difference (no surface ops)
+	thickness = 0.4
+	vee = Translate(Scale(Square(), Vec2(thickness, 1)), Vec2(0, -1))
+	tree = replicate(Scale(Circle(), Vec2(0.75, 0.75)), thickness, vee)
+	fac = 0.75
+	for i in range(6):
+		tree = replicate(Scale(tree, Vec2(fac, fac)), thickness, vee)
 
 	return Output(tree)
